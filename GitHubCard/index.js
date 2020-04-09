2f1f718cea2313dcc16b12e8c0df44ded5f94c7f
ReyWilly <57641631+ReyWilly@users.noreply.github.com>
2020-04-09 12:44:51 -0400

    Finished MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,19 @@
            https://api.github.com/users/<your name>
 */
 
+axios
+
+.get("https://api.github.com/users/ReyWilly")
+
+.then(response => {
+  let myCard = cardMaker(response.data)
+  newCard.appendChild(myCard)
+})
+
+.catch(error => {
+  console.log("Failed! There was an error: ", error);
+})
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,7 +37,13 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"
+];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +64,52 @@ const followersArray = [];
 </div>
 
 */
+function cardMaker(object) {
+
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  let newImg = document.createElement("img");
+  let cardInfo = document.createElement("div");
+  cardInfo.classList.add("card-info");
+
+  let name = document.createElement("h3");
+  name.classList.add("name");
+
+  let username = document.createElement("p");
+  username.classList.add("username");
+
+  let location = document.createElement("p");
+  let profile = document.createElement("p");
+  let userLink = document.createElement("a");
+  let followers = document.createElement("p");
+  let following = document.createElement("p");
+  let cardBio = document.createElement("p");
+
+  newImg.src = object.avatar_url;
+  name.textContent = object.name;
+  username.textContent = object.login;
+  location.textContent = (`Location: ${object.location}`);
+  profile.textContent = (`Profile: ${userLink}`);
+  userLink.textContent = object.html_url;
+  followers.textContent = (`Followers: ${object.followers}`);
+  following.textContent = (`Following: ${object.following}`);
+  cardBio.textContent = (`Bio: ${object.bio}`);
+
+  card.appendChild(newImg);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(cardBio);
+  profile.appendChild(userLink);
+
+  return card;
+}
+let newCard = document.querySelector(".cards");
 
 /* List of LS Instructors Github username's: 
   tetondan
